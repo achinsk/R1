e39ca6a55b724de9936d1ddc8b5577fce6cd2c86
Alex Vorobin <volnyc1@aol.com>
2023-09-03 22:21:01 -0400

    forth commit, updated js file for const spread for total-input class

--- js/script.js
+++ js/script.js
@@ -1,46 +1,17 @@
 'use strict';
 
-// Получить заголовок "Калькулятор верстки" через метод getElementsByTagName. 
-// (тэг h1, получить именно элемент, а не коллекцию)
 const title = document.getElementsByTagName('h1')[0].innerText
-
-// Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName. (класс handler_btn)
 const buttons = document.getElementsByClassName('handler_btn');
 const buttonCalculate = buttons[0]; 
 const buttonReset = buttons[1];
-
-// Получить кнопку "+" под выпадающим списком через метод querySelector. (класс screen-btn)
 const buttonPlus = document.querySelector('.screen-btn')
-
-// Получить все элементы с классом other-items в две разные переменные. 
-// В первую элементы у которых так же присутствует класс percent, 
-// во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
-
-// Получить input type=range через его родителя с классом rollback одним запросом через метод querySelector.
 const rbInput = document.querySelector('.rollback > .main-controls__range > input');
-
-// Получить span с классом range-value через его родителя с классом rollback одним запросом через метод querySelector.
 const rbSpan = document.querySelector('.rollback > .main-controls__range > span');
+const [webLayoutPrice, countScreens, addServicePrice, totalPrice, fullPriceIncludRollBack] = [...document.getElementsByClassName('total-input')];
 
-// Получить все инпуты с классом total-input справа через метод getElementsByClassName. 
-// (класс total-input, получить именно элементы, а не коллекции)
-const allInputs = [...document.getElementsByClassName('total-input')];
-
-// Получить все блоки с классом screen в изменяемую переменную ( let ) 
-// через метод querySelectorAll (далее мы будем переопределять ее значение)
 let screens = document.querySelectorAll('.screen');
-
-// console.log(title);
-// console.log(buttons);
-// console.log(buttonPlus)
-// console.log(otherItemsPercent);
-// console.log(otherItemsNumber); 
-// console.log(rbInput);
-// console.log(rbSpan);
-// console.log(allInputs);
-// console.log(screens);
 
 const appData = {
     title: '',
@@ -84,9 +55,6 @@ const appData = {
                 x = prompt("Сколько это будет стоить?");
             } while (!appData.isNumber(x));
 
-            // done by simply concat name to id. Dash is needed to make it unique, otherwise for example:
-            // "name1" + 1  is equal to "name" + 11
-            // could also be done like Object of Objects {{},{}....} or Object of keys with Array {1: [name, key], ...} 
             appData.additionalServices[(name + "-" + i)] = parseFloat(x);                          
         }
             
@@ -101,7 +69,6 @@ const appData = {
         }
     },
 
-    // Answer prompt string is also checked for null, empty and all spaces as invalid
     isProperString: function(prompt_string) {
         if (prompt_string == null || prompt_string == "" || prompt_string.trim().length == 0) {
             return false;
